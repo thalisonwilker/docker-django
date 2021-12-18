d81182f263710c6bdd91ab85641653dd59afaadd
Thalyson Wilker <thalison.wilker@gmail.com>
2021-12-17 21:33:44 -0300

    settins: adicionar usuários e model de login

--- api/config/settings.py
+++ api/config/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users'
 ]
 
 MIDDLEWARE = [
@@ -138,3 +139,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'users.User'
